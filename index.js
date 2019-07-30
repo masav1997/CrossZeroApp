d25edb2a5bbf1daf1d0abc6f1c3501c11b7f4d73
Anna <47542228+masav1997@users.noreply.github.com>
2019-07-30 19:49:25 +0300

    The game is done!

---
+++ index.js
@@ -0,0 +1,47 @@
+window.addEventListener('load', function () {
+    var button = document.querySelector('.startNewGame');
+    var field = document.querySelector('.field');
+    var cells = document.querySelectorAll('.cell');
+    var winnerMessage = document.querySelector('.winner-message');
+    var nextMove = 'x';
+
+    button.addEventListener('click', function startNewGame() {
+        for (var i=0; i < cells.length; i++) {
+            cells[i].classList.remove('x');
+            cells[i].classList.remove('o');
+        }
+        winnerMessage.innerHTML('');
+    });
+    field.addEventListener('click', function handleCellClick(e) {
+        if (!e.target.classList.contains('cell') || (e.target.classList.contains('x'))
+            || (e.target.classList.contains('o'))
+        ){
+            return;
+        }
+
+        if (getWinner()){
+            return;
+        }
+
+        e.target.classList.add(nextMove);
+        if (nextMove === 'x') {
+            nextMove = 'o';
+        }
+        else {
+            nextMove = 'x';
+        }
+
+        var winner = getWinner();
+        if (winner) {
+            if (winner === 'x') {
+                winnerMessage.innerHTML = 'The 1-st player is win!';
+                document.querySelector('.winner-message').style.color = "red";
+            }
+            else {
+                winnerMessage.innerHTML = 'The 2-nd player is win!';
+                document.querySelector('.winner-message').style.color = "blue";
+            }
+        }
+    })
+
+});
